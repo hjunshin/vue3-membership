046fee8f79367eaa2c50ddaf8c5172c7f52aee6d
HJUN <guswns823@gmail.com>
2022-07-22 22:08:15 +0900

    회원가입 완료 레이아웃 수정 및 스타일 추가

--- src/components/MemberRegistrationCompleted.vue
+++ src/components/MemberRegistrationCompleted.vue
@@ -11,20 +11,26 @@
 <template>
   <div class="member-step" v-if="steps === 4">
     <h2>회원가입 완료</h2>
-    <p>
-      '{{info.name}}'님<br />
-      회원가입 되었습니다.
-    </p>
-    <dl>
-      <dt>이메일:</dt>
-      <dd>{{info.email}}</dd>
-      <dt>주소:</dt>
-      <dd>
-        {{info.address}}<br />
-        {{info.detailAddress}}
-      </dd>
-      <dt>연락처:</dt>
-      <dd>{{info.cellphone}}</dd>
-    </dl>
+    <div class="member-complete">
+      <p>
+        '{{info.name}}'님<br />
+        회원가입 되었습니다.
+      </p>
+      <dl>
+        <dt>이메일:</dt>
+        <dd>{{info.email}}</dd>
+      </dl>
+      <dl>
+        <dt>주소:</dt>
+        <dd>
+          {{info.address}}<br />
+          {{info.detailAddress}}
+        </dd>
+      </dl>
+      <dl>
+        <dt>연락처:</dt>
+        <dd>{{info.cellphone}}</dd>
+      </dl>
+    </div>
   </div>
 </template>
